3d66eac6ee0cc0c0bd750299c98b3c7c78878bf0
abhinand8113 <117014023+abhinand8113@users.noreply.github.com>
2023-02-28 17:14:56 +0530

    files added

--- app/src/main/java/com/example/nfcattendance/Register.java
+++ app/src/main/java/com/example/nfcattendance/Register.java
@@ -1,14 +1,32 @@
 package com.example.nfcattendance;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_register);
+    }
+
+    public void RegisterPrincipal(View v)
+    {
+        startActivity(new Intent(this, RegisterPrincipal.class));
+    }
+    public void RegisterHOD(View v)
+    {
+        startActivity(new Intent(this, RegisterHOD.class));
+    }
+    public void RegisterTeacher(View v)
+    {
+        startActivity(new Intent(this, TeacherRegister.class));
     }
 }
---
+++ app/src/main/java/com/example/nfcattendance/TeacherRegister.java
@@ -0,0 +1,17 @@
+package com.example.nfcattendance;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeacherRegister  extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.teacherregister);
+    }
+}
